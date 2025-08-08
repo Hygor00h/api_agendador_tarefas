cdcf7c8fbbc414db9bb618aa5520b0f6faf70527
Hygor <hygorbr28@gmail.com>
2025-08-08 20:29:54 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/javanauta/api_agendador_tarefas/ApiAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.api_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/api_agendador_tarefas/ApiAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.api_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

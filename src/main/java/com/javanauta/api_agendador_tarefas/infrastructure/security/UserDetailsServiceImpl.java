c5e2edb092cc995ef8be38bd83cacf6dffddf953
Hygor <hygorbr28@gmail.com>
2025-08-09 11:56:29 -0300

    correção de apontamento

--- src/main/java/com/javanauta/api_agendador_tarefas/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/javanauta/api_agendador_tarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -3,14 +3,12 @@ package com.javanauta.api_agendador_tarefas.infrastructure.security;
 
 import com.javanauta.api_agendador_tarefas.business.dto.UsuarioDTO;
 import com.javanauta.api_agendador_tarefas.infrastructure.client.UsuarioClient;
-import com.javanauta.apiusuario.infrastructure.entity.Usuario;
-import com.javanauta.apiusuario.infrastructure.repository.UsuarioRepository;
-import lombok.RequiredArgsConstructor;
+//import com.javanauta.apiusuario.infrastructure.entity.Usuario;
+//import com.javanauta.apiusuario.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 @Service
